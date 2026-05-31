2e882965ef3d068cd9e78c54a5d1d4e01208db06
Afton Rhodes <aftonrhodes@Aftons-MacBook-Pro.local>
2026-05-31 13:32:24 +0100

    Updated tests

--- dashboard_page.py
+++ dashboard_page.py
@@ -1,13 +1,25 @@
-# dashboard_page.py
+    # dashboard_page.py
 from playwright.sync_api import Page
 
 class DashboardPage:
     def __init__(self, page: Page):
         self.page = page
-        self.title = page.locator("h1")
+        # Selectors
+        self.table_rows = page.locator("#datatable tbody tr")
+        self.name_input = page.locator("#input-name")
+        self.email_input = page.locator("#input-email")
+        self.submit_btn = page.locator("#submit-button")
+        self.form_msg = page.locator("#form-message")
+        self.status_checkbox = page.locator("#status-checkbox")
+        self.status_label = page.locator("#status-label")
 
     def navigate(self):
         self.page.goto("https://afton-quality-dashboard.s3.us-east-1.amazonaws.com/index.html")
-    
-    def get_heading_text(self):
-        return self.page.inner_text("h1")
+
+    def get_table_row_count(self):
+        return self.table_rows.count()
+
+    def submit_form(self, name, email):
+        self.name_input.fill(name)
+        self.email_input.fill(email)
+        self.submit_btn.click()
